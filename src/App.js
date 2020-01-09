32ec48766e4c2fab8f7ad2f09f6307ffb9d276b9
Andres Quan <aquan@lanl.gov>
2020-01-09 11:40:06 -0700

    Fix parallel coordinate chart to use real data

--- src/App.js
+++ src/App.js
@@ -121,22 +121,100 @@ class App extends React.Component {
 }
 
 {/* The Parallel Coordinate Chart */}
-function ParallelCoordinateView() {
+function ParallelCoordinateView(props) {
+	var chi = []
+	var chi_increment = []
+	var count_cutoff = []
+	var detector_distance = []
+	var exposure_period = []
+	var exposure_time = []
+	var number_of_oscillations = []
+	var phi = []
+	var phi_increment = []
+	var start_angle = []
+	var tau = []
+	var threshold_setting = []
+	var wavelength = []
+	for (const point in props.csvData){
+		chi.push(props.csvData[point][" Chi"])
+		chi_increment.push(props.csvData[point][" Chi increment"])
+		count_cutoff.push(props.csvData[point][" Count cutoff"])
+		detector_distance.push(props.csvData[point][" Detector Distance"])
+		exposure_period.push(props.csvData[point][" Exposure period"])
+		number_of_oscillations.push(props.csvData[point][" Number of Oscillations"])
+		phi.push(props.csvData[point][" Phi"])
+		phi_increment.push(props.csvData[point][" Phi increment"])
+		start_angle.push(props.csvData[point][" Start Angle"])
+		tau.push(props.csvData[point][" Tau"])
+		threshold_setting.push(props.csvData[point][" Threshold setting"])
+		wavelength.push(props.csvData[point][" Wavelength"])
+	}
+
+	let chi_max = Math.max(chi.map(Number))
+	let chi_min = Math.min(chi.map(Number))
+
 	var trace = {
-		type: 'parcords',
+		type: 'parcoords',
 		line: {
 			color: 'blue'
 		},
 
-		dimensions: []
-	}
+		dimensions: [{
+			label: "Chi",
+			values: chi
+		},
+		{
+			label: "Chi Increment",
+			values: chi_increment
+		},
+		{
+			label: "Detector Distance",
+			values: detector_distance
+		},
+		{
+			label: "Exposure Period",
+			values: exposure_period
+		},
+		{
+			label: "Exposure Time",
+			values: exposure_time
+		},
+		{
+			label: "Number of Oscillations",
+			values: number_of_oscillations
+		},
+		{
+			label: "Phi",
+			values: phi
+		},
+		{
+			label: "Phi Increment",
+			values: phi_increment
+		},
+		{
+			label: "Tau",
+			values: tau
+		},
+		{
+			label: "Threshold",
+			values: threshold_setting
+		},
+		{
+			label: "Wavelength",
+			values: wavelength
+		},
+		
+		]
+	};
+
 	return (
 		<div className="ParallelCoordinate">
 			<Plot
-				data={{}}
+				data={[trace]}
 				layout={ {title: 'Parallel Coordinate Plot',
 						autosize: true,
-						margin: {l: 50, r: 50, b: 50, t: 70, pad: 4}		
+						margin: {l: 50, r: 50, b: 30, t: 80, pad: 4},
+						y: 0
 				} }
 				useResizeHandler={true}
 				style={ {height:"100%",width:"100%"} }
